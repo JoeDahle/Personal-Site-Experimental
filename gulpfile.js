c812c7b77b2c77d96f70859f9d8717778faca3d5
Joe Dahle <joe.dahle@gmail.com>
2016-04-29 13:40:01 -0700

    Chore: Create and setup gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var pages = require('gulp-gh-pages');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+gulp.task('deploy', function(){
+  return gulp.src('./')
+    .pipe(pages());
+});
+
+gulp.task('serve', function(){
+  browserSync.init({
+    server: './',
+    port: 3030
+  })
+});
+
+gulp.task('sass', function(){
+  return gulp.src('scss/style.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('default', ['serve', 'sass'], function(){
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch(['index.html', 'js/*.js', 'styles/style.css'], browserSync.reload);
+});
