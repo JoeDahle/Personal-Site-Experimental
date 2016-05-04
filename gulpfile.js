6653d9e29254ae3ec48ed56f14ce79cf3c6c87b2
Joe Dahle <joe.dahle@gmail.com>
2016-05-03 21:24:00 -0700

    chore: Add gulp-responsive-images and config

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var pages = require('gulp-gh-pages');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
+var responsive = require('gulp-responsive-images');
 
 gulp.task('deploy', function(){
   return gulp.src('./')
@@ -19,6 +20,25 @@ gulp.task('sass', function(){
   return gulp.src('scss/style.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('styles'));
+});
+
+gulp.task('image', function(){
+  gulp.src('assetPool/**/*')
+    .pipe(responsive({
+      'background.jpg': [{
+        width: 2000,
+        suffix: '-2000'
+      },
+      {
+        width: 1000,
+        suffix: '-1000'
+      },
+      {
+        width: 500,
+        suffix: '-500'
+      }]
+    }))
+    .pipe(gulp.dest('assets/'));
 });
 
 gulp.task('default', ['serve', 'sass'], function(){
