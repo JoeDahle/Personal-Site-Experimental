46ee6e8e83ba8ad7bb8577e3a0283a76305983ae
Joe Dahle <joe.dahle@gmail.com>
2016-05-03 21:09:08 -0700

    feat: Complete navbar functionality

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,11 @@
 
 class navBar {
   constructor () {
+    // element selectors
     this.toggleMenuButtonEl = document.querySelector('.js-menu');
     this.navbarEl = document.querySelector('.js-navigator');
 
+    // bindings
     this.toggleAnimation = this.toggleAnimation.bind(this);
     this.toggleNavbar = this.toggleNavbar.bind(this);
 
@@ -14,17 +16,19 @@ class navBar {
   addEventListeners () {
     this.toggleMenuButtonEl.addEventListener('click', this.toggleAnimation);
     this.toggleMenuButtonEl.addEventListener('click', this.toggleNavbar);
+
+    // TODO: add event listener to close header when clicking outside
+    // the navigation. Will be done, once more elements are added.
   }
 
-/**
- * Toggle the visibility of the navigation bar
- */
+  /**
+   * Toggle the visibility of the navigation bar
+   */
   toggleNavbar () {
     this.navbarEl.classList.toggle('navigator--collapse');
     this.navbarEl.classList.toggle('navigator--expand');
   }
 
-// TODO: add delay to content transition
   toggleAnimation () {
     this.toggleMenuButtonEl.classList.toggle('menu__toggle--animate');
     if (this.toggleMenuButtonEl.classList.contains('menu__toggle--animate')) {
