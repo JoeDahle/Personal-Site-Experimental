26ba881759d835fc297b000396fee8afa52f7fb4
Joe Dahle <joe.dahle@gmail.com>
2016-05-07 20:17:54 -0700

    feat: Allow navbar dismissal from background elements

--- js/nav.js
+++ js/nav.js
@@ -5,10 +5,12 @@ class navBar {
     // element selectors
     this.toggleMenuButtonEl = document.querySelector('.js-menu');
     this.navbarEl = document.querySelector('.js-navigator');
+    this.backgroundEl = document.querySelector('.intrigue__container');
 
     // bindings
     this.toggleAnimation = this.toggleAnimation.bind(this);
     this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.hideNavbar = this.hideNavbar.bind(this);
 
     this.addEventListeners();
   }
@@ -16,6 +18,7 @@ class navBar {
   addEventListeners () {
     this.toggleMenuButtonEl.addEventListener('click', this.toggleAnimation);
     this.toggleMenuButtonEl.addEventListener('click', this.toggleNavbar);
+    this.backgroundEl.addEventListener('click', this.hideNavbar);
 
     // TODO: add event listener to close header when clicking outside
     // the navigation. Will be done, once more elements are added.
@@ -27,6 +30,11 @@ class navBar {
   toggleNavbar () {
     this.navbarEl.classList.toggle('navigator--collapse');
     this.navbarEl.classList.toggle('navigator--expand');
+  }
+
+  hideNavbar () {
+    this.navbarEl.classList.remove('navigator--expand');
+    this.navbarEl.classList.add('navigator--collapse');
   }
 
   toggleAnimation () {
